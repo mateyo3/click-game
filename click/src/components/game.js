205b87c1aae2210209245e1d1980cec34b05642d
mateyo3 <mateoreyes720@gmail.com>
2018-01-27 00:05:07 -0800

    onClick added to images to increase score

--- click/src/components/game.js
+++ click/src/components/game.js
@@ -28,6 +28,7 @@ let images = Images;
     // handles score increase
     handleIncreaseScore  = () => {
         this.setState({ score: this.state.score + 1 });
+        console.log("Score: " + this.state.score);
       }
 
     // handleSetTopScore  = () => {
@@ -40,19 +41,19 @@ let images = Images;
     render() {
       return (
     	<div className="game-containter">
-        <img src={useImage[0]} height="200" width="200" />
-        <img src={useImage[1]} height="200" width="200" />
-        <img src={useImage[2]} height="200" width="200" />
-        <img src={useImage[3]} height="200" width="200" />
-        <img src={useImage[4]} height="200" width="200" />
-        <img src={useImage[5]} height="200" width="200" />
-        <img src={useImage[6]} height="200" width="200" />
-        <img src={useImage[7]} height="200" width="200" />
-        <img src={useImage[8]} height="200" width="200" />
-        <img src={useImage[9]} height="200" width="200" />
-        <img src={useImage[10]} height="200" width="200" />
-        <img src={useImage[11]} height="200" width="200" />
-        <img src={useImage[12]} height="200" width="200" />
+        <img src={useImage[0]} alt={useImage[0]} onClick={this.handleIncreaseScore} id="0" height="200" width="200" />
+        <img src={useImage[1]} alt={useImage[1]} onClick={this.handleIncreaseScore} id="1" height="200" width="200" />
+        <img src={useImage[2]} alt={useImage[2]} onClick={this.handleIncreaseScore} id="2" height="200" width="200" />
+        <img src={useImage[3]} alt={useImage[3]} onClick={this.handleIncreaseScore} id="3" height="200" width="200" />
+        <img src={useImage[4]} alt={useImage[4]} onClick={this.handleIncreaseScore} id="4" height="200" width="200" />
+        <img src={useImage[5]} alt={useImage[5]} onClick={this.handleIncreaseScore} id="5" height="200" width="200" />
+        <img src={useImage[6]} alt={useImage[6]} onClick={this.handleIncreaseScore} id="6" height="200" width="200" />
+        <img src={useImage[7]} alt={useImage[7]} onClick={this.handleIncreaseScore} id="7" height="200" width="200" />
+        <img src={useImage[8]} alt={useImage[8]} onClick={this.handleIncreaseScore} id="8" height="200" width="200" />
+        <img src={useImage[9]} alt={useImage[9]} onClick={this.handleIncreaseScore} id="9" height="200" width="200" />
+        <img src={useImage[10]} alt={useImage[10]} onClick={this.handleIncreaseScore} id="10" height="200" width="200" />
+        <img src={useImage[11]} alt={useImage[11]} onClick={this.handleIncreaseScore} id="11" height="200" width="200" />
+        <img src={useImage[12]} alt={useImage[12]} onClick={this.handleIncreaseScore} id="12" height="200" width="200" />
         
     	</div>
 
