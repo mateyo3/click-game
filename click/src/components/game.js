1de65076b89910397a0b2a97f65ae4487163552f
mateyo3 <mateoreyes720@gmail.com>
2018-01-26 23:33:41 -0800

    shuffle added

--- click/src/components/game.js
+++ click/src/components/game.js
@@ -1,71 +1,77 @@
 import React, { Component } from 'react';
 import Images from '../image.json';
-// import { Link } from 'react-router';
 
-// function shuffleCor(array) {
-//   let i = Images.length - 1;
-//   for (; i > 0; i--) {
-//     const j = Math.floor(Math.random() * (i + 1));
-//     const temp = Images[i];
-//     Images[i] = Images[j];
-//     Images[j] = temp;
-//   }
-//   return randomCorgi;
-// }
+const shuffle = require('shuffle-array');
 
 
-class Game extends Component {
-  // Setting this.state.score to the score json array
-  state = {
-    score: 0,
-    topScore: 0,
-    click: false,
-    image: ""
+let images = Images;
+  for (let i = 0; i < images.length; i++) {
+    // images[i]  = image[Math.floor(Math.random()*image.length)];
+    images[i] = images[i].imgURL;
   }
 
-  //handles score increase 
-  handleIncreaseScore  = () => {
-      this.setState({ score: this.state.score + 1 });
-    }
+ let useImage = shuffle(images);
+  console.log(useImage);
 
-  handleSetTopScore  = () => {
-      this.setState({ score: this.state.score + 1 });
-    }
 
-  handleBtnClick = event => {
-    const {name, value} = event.target;
-
-    this.setState({
-      click: true,
-      score: handleIncreaseScore()
-      // topScore:
-      //   if (score > topScore) {
-      //     this.setState({ this.state.topScore = score })
-      //   }
-    });
-
-  }
-
+  class Game extends Component {
+    // Setting this.state.score to the score json array
+    state = {
+      score: 0,
+      topScore: 0,
+      click: [],
+      image: Images
+    };
   
+    //handles re-shuffle
 
-  
+    // handles score increase
+    handleIncreaseScore  = () => {
+        this.setState({ score: this.state.score + 1 });
+      }
 
-  render() {
-    return (
-  	<div className="game-containter">
-  		<img src={"https://github.com/mateyo3/click-game/blob/master/click/public/assets/images/corgi-pup-01.jpg?raw=true"} />
-  	</div>
-    );
+    // handleSetTopScore  = () => {
+    //     this.setState({ score: this.state.score + 1 });
+    //   }
+    
+
+    
+
+    render() {
+      return (
+    	<div className="game-containter">
+        <img src={useImage[0]} height="200" width="200" />
+        <img src={useImage[1]} height="200" width="200" />
+        <img src={useImage[2]} height="200" width="200" />
+        <img src={useImage[3]} height="200" width="200" />
+        <img src={useImage[4]} height="200" width="200" />
+        <img src={useImage[5]} height="200" width="200" />
+        <img src={useImage[6]} height="200" width="200" />
+        <img src={useImage[7]} height="200" width="200" />
+        <img src={useImage[8]} height="200" width="200" />
+        <img src={useImage[9]} height="200" width="200" />
+        <img src={useImage[10]} height="200" width="200" />
+        <img src={useImage[11]} height="200" width="200" />
+        <img src={useImage[12]} height="200" width="200" />
+        
+    	</div>
+
+
+      );
+    }
   }
-}
 
 
-// const Game = () => (
-//     <div className="game-containter">
-//       <img src={"https://github.com/mateyo3/click-game/blob/master/click/public/assets/images/corgi-pup-01.jpg?raw=true"} alt="corgi-pup-01" className="img-responsive"/>
-//     </div>
-// )
+  // const Game = () => (
+  //     <div className="game-containter">
+  //       <img src={"https://github.com/mateyo3/click-game/blob/master/click/public/assets/images/corgi-pup-01.jpg?raw=true"} alt="corgi-pup-01" className="img-responsive"/>
+  //     </div>
+
+
 
 
 export default Game;
 
+
+
+        
--- click/src/components/header.js
+++ click/src/components/header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 
 const Header = () => (
-	<header className="jumbotron">
+	<header className="header">
 		<h1 className="text-center">Memory Game</h1>
-		<b><p className="text-center text-danger">Click an image to earn points, but don't click on the same image twice!</p></b>
+		<b><p className="text-center text-danger">Click a Corgi puppy to earn points, but don't click on the same Corgi twice!</p></b>
 
 	</header>
 );
