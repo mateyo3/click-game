47371254660c5f82db9fb138f7bab41a16b43e99
mateyo3 <mateoreyes720@gmail.com>
2018-01-28 21:33:36 -0800

    readme

--- click/src/components/header.js
+++ click/src/components/header.js
@@ -3,9 +3,7 @@ import React from "react";
 
 const Header = () => (
 	<header className="header">
-		<h1 className="text-center">Memory Game</h1>
-		<b><p className="text-center text-danger">Click a Corgi puppy to earn points, but don't click on the same Corgi twice!</p></b>
-
+		<h1 className="text-center">SIT, STAY, PLAY!</h1>
 	</header>
 );
 
--- click/src/components/navbar.js
+++ click/src/components/navbar.js
@@ -9,19 +9,19 @@ class Navbar extends Component {
     topScore: 0
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+  // removeFriend = id => {
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   const friends = this.state.friends.filter(friend => friend.id !== id);
+  //   // Set this.state.friends equal to the new friends array
+  //   this.setState({ friends });
+  // };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
  	<div className="navbar">
- 		<span>Memory Game!</span>
- 		<span className="message">Let's Play!</span>
+ 		<span>Memory Game: Click a Corgi puppy to earn points, but don't click on the same Corgi twice!</span>
+ 		<span className="message">It's Play Time!</span>
  	</div>
     );
   }
